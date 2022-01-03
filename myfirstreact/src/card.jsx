002d02bfa253ca4c57fd26be977afc1005b445ee
shaiksaif279 <73707432+shaiksaif279@users.noreply.github.com>
2022-01-03 20:41:46 +0530

    Add files via upload

---
+++ myfirstreact/src/card.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class card extends React.Component {
+    render() { 
+        return <div className="card">
+            
+        </div>;
+    }
+}
+ 
+export default card;
